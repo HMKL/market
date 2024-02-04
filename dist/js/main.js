af3817689fae10d1c4f34ff31ad130b4d218f1f1
Catalin B <catalin.bac@gmail.com>
2024-02-04 10:39:06 +0200

    some modifications

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,61 @@
 let layoutHandler;
 
-(function($) {
+(function ($) {
     'use strict';
-    let LayoutHandler = function() {
+    let LayoutHandler = function () {
         let self = this;
 
-        this.ready = function() {
+        this.ready = function () {
             this.handleDOM();
             this.handleEvents();
         };
 
-        this.handleDOM = function() {
+        this.handleDOM = function () {
 
         };
 
-        this.handleEvents = function() {
+        this.handleEvents = function () {
 
         };
     };
 
     layoutHandler = new LayoutHandler();
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         layoutHandler.ready();
+
+
+
+
+
+
+        const progressCircle = document.querySelector(".autoplay-progress svg");
+        const progressContent = document.querySelector(".autoplay-progress span");
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 30,
+            centeredSlides: true,
+            // autoplay: {
+            //     delay: 2500,
+            //     disableOnInteraction: false
+            // },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            },
+            on: {
+                autoplayTimeLeft(s, time, progress) {
+                    progressCircle.style.setProperty("--progress", 1 - progress);
+                    progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+                }
+            }
+        });
+
+
+
 
 
     });
@@ -44,7 +77,6 @@ let layoutHandler;
     })
   }
   addClassHamburger();
-  console.log(getSideBar)
 })();
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,7 +14,6 @@
     })
   }
   addClassHamburger();
-  console.log(getSideBar)
 })();
 
 
