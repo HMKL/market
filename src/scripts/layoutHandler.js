50ba48430aee4179c2739882140616d87ac0bf1a
Catalin B <catalin.bac@gmail.com>
2024-03-02 21:11:38 +0200

    additional changes on swiper

--- src/scripts/layoutHandler.js
+++ src/scripts/layoutHandler.js
@@ -34,10 +34,10 @@ let layoutHandler;
         var swiper = new Swiper(".mySwiper", {
             spaceBetween: 30,
             centeredSlides: true,
-            // autoplay: {
-            //     delay: 2500,
-            //     disableOnInteraction: false
-            // },
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false
+            },
             pagination: {
                 el: ".swiper-pagination",
                 clickable: true
